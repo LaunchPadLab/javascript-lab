6c927b2b1c293f2d1900287a4c8492b5d5aca2ac
bhennes2 <bhennes2@gmail.com>
2013-05-02 14:07:54 -0500

    Remove submodule

---
+++ README.md
@@ -0,0 +1,12 @@
+javascript-lab
+==============
+
+Our Javascript Lab - Learn, Code, Build
+
+Lab #1 - We're going to wire up a simple four function calculator in Javascript using a bit of jQuery and good ole Javascript. Fork this repository to pull down lesson #1 and let's get coding.
+
+Lab #2 - We'll dive into a new example using jQuery, jQuery-UI and javascript on a sign up form. This example will focus on showing and hiding elements on a page and using jQuery UI's datepicker and autocomplete widgets.
+
+Brendan Hennessy
+
+LaunchPad Lab
---
+++ lab#1/js/calculator.js
@@ -0,0 +1,128 @@
+var Calculator = function() {
+    this.total = 0;
+    this.current = 0;
+    this.operation = null;
+    this.fractionExp = 0;
+};
+
+Calculator.prototype = {
+	
+    add: function() {
+        this.calculate();
+        this.operation = function(a, b) {
+            return a + b;
+        };
+        this.setCurrent(0, "");
+        this.fractionExp = 0; // decimal fix
+    },
+    subtract: function() {
+        this.calculate();
+        this.operation = function(a, b) {
+            return a - b;
+        };
+        this.setCurrent(0, "");
+        this.fractionExp = 0; // decimal fix
+    },
+    multiply: function() {
+        this.calculate();
+        this.operation = function(a, b) {
+            return a * b;
+        };
+        this.setCurrent(0, "");
+        this.fractionExp = 0; // decimal fix
+    },
+    divide: function() {
+        this.calculate();
+        this.operation = function(a, b) {
+            return a / b;
+        };
+        this.setCurrent(0, "");
+        this.fractionExp = 0; // decimal fix
+    },
+    clear: function() {
+        this.setCurrent(0, "");
+        this.fractionExp = 0; // decimal fix
+    },
+    allClear: function() {
+        this.clear();
+        this.total = 0;
+        this.operation = null;
+        console.log("total: " + this.total);
+        this.fractionExp = 0; // decimal fix
+    },
+    calculate: function() {
+        if (this.operation) {
+            this.total = this.operation(this.total, this.current);
+        } else {
+            this.total = this.current;
+        }
+        console.log("total: " + this.total);
+    },
+    equals: function() {
+        this.calculate();
+        this.updateDisplay(this.total);
+    },
+    digit: function(number) {
+        // shift current left, add number       
+        // this is the key the decimal point
+        if (this.fractionExp) {
+            // not corrent but close
+            this.setCurrent(this.current + (number / Math.pow(10, this.fractionExp)));
+            this.fractionExp = this.fractionExp + 1
+        } else {            
+            this.setCurrent((this.current * 10) + number);
+        };
+    
+    },
+    point: function() {
+        this.fractionExp = 1;
+    },
+    setCurrent: function(value, text) {
+        this.current = value;
+        this.updateDisplay(typeof text == "undefined" ? this.current : text);
+        console.log("current: " + this.current);
+    },
+    updateDisplay: function(value) {
+        $("#display").text(value);
+    }
+};
+
+var calc = new Calculator();
+
+$("li").click(function() {
+   var raw = $(this).text()
+   number = parseInt(raw, 10);
+   if (isNaN(number)) {
+      // must be an operator  
+      switch (raw) {
+      case "C":
+          calc.clear();
+          break;
+      case "AC":
+          calc.allClear();
+          break;
+      case "x":
+          calc.multiply();
+          break;
+      case "/":
+          calc.divide();
+          break;
+      case "+":
+          calc.add();
+          break;
+      case "-":
+          calc.subtract();
+          break;
+      case "=":
+          calc.equals();
+          break;
+      case ".":
+          calc.point();
+          break;
+      }
+   } 
+	else {
+     // its a digit
+   	calc.digit(number);
+   }
+});
